54ee4a2d9285ed8224730ea00735f89f910e4738
rray524 <rray524@gmail.com>
2023-01-25 20:39:58 +0600

    fixed required field

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import * as yup from "yup";
 function App() {
 
   const schema = yup.object({
-    username: yup.string().required("Username is required"),
-    phoneNum: yup.string().required().matches(/^(0|91)?[6-9][0-9]{9}$/, "Phone Number is not valid"),
+    username: yup.string().required("User Name is required"),
+    phoneNum: yup.string().required("Phone Number is required").matches(/^(0|91)?[6-9][0-9]{9}$/, "Phone Number is not valid"),
     emailId: yup.string().required("Email is required").email("Email is not valid"),
-    password: yup.string().min(6, "Password must be at least 6 characters long"),
+    password: yup.string().required("Password Field is required").min(6, "Password must be at least 6 characters long"),
     confirmPassword: yup.string().oneOf([yup.ref("password")], "Password must match"),
 
   })
@@ -26,12 +26,19 @@ function App() {
       <h1>Sign Up</h1>
       <p>Please create a account to get in our system. Thank you!</p>
       <form onSubmit={handleSubmit(onSubmit)}>
+
         <Input label="User Name" id="username" type="text" placeholder="User Name" register={{ ...register("username") }} errorMessage={errors.username?.message} />
+
         <Input label="Phone Number" id="phoneNum" type="text" placeholder="Phone Number" register={{ ...register("phoneNum") }} errorMessage={errors.phoneNum?.message} />
+
         <Input label="Email" id="emailId" type="email" placeholder="Enter Email" register={{ ...register("emailId") }} errorMessage={errors.emailId?.message} />
+
         <Input label="Password" id="password" type="password" placeholder="Enter Password" register={{ ...register("password") }} errorMessage={errors.password?.message} />
+
         <Input label="Confirm Password" id="confirmPassword" type="password" placeholder="Confirm Password" register={{ ...register("confirmPassword") }} errorMessage={errors.confirmPassword?.message} />
+
         <button>Sign Up</button>
+
       </form>
 
     </div>
